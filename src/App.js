c8d646bcf6d5435ef1a4d3bb6089ad4336d432e4
zainulabedin23 <abedinz427@gmail.com>
2024-01-04 11:53:38 +0530

    real time chat application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const googleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        setUser({name:result.user.displayName,email: result.user.email});
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  const signOut=()=>signOut(auth).then(() => {
+    setUser("");
+  }).catch((error) => {
+    console.error("Signout Error", error);
+  });
+  
+  const updateHeight = () => {
+    const el = document.getElementById("chat");
+    if (el) {
+      el.scrollTop = el.scrollHeight;
+    }
+  };
+  //
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      // closure concept
+      setChats((chats) => [...chats, data.val()]);
+      setTimeout(() => {
+        updateHeight();
+      }, 100);
+    });
+  }, []);
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    // chatref ek empty chat hai jisko
+    // aapko push karna hai hatListRef
+    set(chatRef, {
+      user,
+      message: msg,
+    });
+    setMsg("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user.email ? null: (
+        <div className="login-div">
+          {/* <input
+            type="text"
+            placeholder="Enter name to start"
+            onBlur={(e) => setName(e.target.value)}
+          ></input> */}
+
+          <button className="button-1" onClick={e=>{googleLogin()} }>login</button>
+        </div>
+      )}
+      {user.email ? (
+        <div>
+          <h3>User: {user.name}</h3>
+          <div className="chat-container" id="chat">
+            {chats.map((c, i) => (
+              <div
+                key={i}
+                className={`container ${c.user.email === user.email ? "me" : ""}`}
+              >
+                <p className="chatbox">
+                  <strong>{c.user.name}: </strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="btm">
+            <input
+              type="text"
+              placeholder="enter your text"
+              onInput={(e) => setMsg(e.target.value)}
+              value={msg}
+            />
+            <button onClick={(e) => sendChat()} className="button-3">
+              send
+            </button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBTniwnMhXKLY2Tpje9xiArbrQnk1o7QBQ",
+  authDomain: "react-chat-app-c5a69.firebaseapp.com",
+  databaseURL: "https://react-chat-app-c5a69-default-rtdb.firebaseio.com",
+  projectId: "react-chat-app-c5a69",
+  storageBucket: "react-chat-app-c5a69.appspot.com",
+  messagingSenderId: "934054466469",
+  appId: "1:934054466469:web:54b9f135fa171bc5d60e4c",
+  measurementId: "G-4KJYDMW92E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
